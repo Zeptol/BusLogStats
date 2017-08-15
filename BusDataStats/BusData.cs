3a3fee8ac1c96eb6ee82d37df4940bf34d9a519a
Xyf <635653084@qq.com>
2017-08-15 17:49:41 +0800

    initial commit

---
+++ BusDataStats/BusData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace BusDataStats
+{
+    public class BusData
+    {
+        public string LineCode { get; set; }
+        public Direction Direction { get; set; }
+        public int StationNum { get; set; }
+        public string StationCode { get; set; }
+        public string VehCode { get; set; }
+        public DateTime EstimatedTime { get; set; }
+        public DateTime ProcessTime { get; set; }
+    }
+
+    public class BusDataStats
+    {
+        public string LineCode { get; set; }
+        public Direction Direction { get; set; }
+        public int StationNum { get; set; }
+        public string StationCode { get; set; }
+        public string VehCode { get; set; }
+        public DateTime FirstEstimatedTime { get; set; }
+        public DateTime LastEstimatedTime { get; set; }
+        public DateTime FirstProcessTime { get; set; }
+        public DateTime LastProcessTime { get; set; }
+
+        public double TimeDiffMins
+        {
+            get
+            {
+                var res = (LastProcessTime - FirstEstimatedTime).TotalMinutes;
+                return -(Math.Abs(res) > 1000 ? res < 0 ? res + 1440 : res > 0 ? res - 1440 : res : res);
+                //return - (res < -1000 ? res + 1440 : res);
+            }
+        }
+        public double ActualTotalMins
+        {
+            get
+            {
+                var res = (LastProcessTime - FirstProcessTime).TotalMinutes;
+                return res < -1000 ? res + 1440 : res;
+            }
+        }
+    }
+    public enum Direction
+    {
+        Up,
+        Down
+    }
+}
---
+++ BusDataStats/Index.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace BusDataStats
+{
+    partial class Index
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewStats = new System.Windows.Forms.DataGridView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridViewTotal = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dataGridViewMatrix = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStats)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTotal)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMatrix)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewStats
+            // 
+            this.dataGridViewStats.AllowUserToAddRows = false;
+            this.dataGridViewStats.AllowUserToDeleteRows = false;
+            this.dataGridViewStats.AllowUserToOrderColumns = true;
+            this.dataGridViewStats.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewStats.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStats.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewStats.Name = "dataGridViewStats";
+            this.dataGridViewStats.ReadOnly = true;
+            this.dataGridViewStats.RowTemplate.Height = 23;
+            this.dataGridViewStats.Size = new System.Drawing.Size(545, 339);
+            this.dataGridViewStats.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(-2, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(553, 368);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridViewStats);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(545, 342);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "统计";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridViewTotal);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(545, 342);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "总体统计";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewTotal
+            // 
+            this.dataGridViewTotal.AllowUserToAddRows = false;
+            this.dataGridViewTotal.AllowUserToDeleteRows = false;
+            this.dataGridViewTotal.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewTotal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTotal.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewTotal.Name = "dataGridViewTotal";
+            this.dataGridViewTotal.ReadOnly = true;
+            this.dataGridViewTotal.RowTemplate.Height = 23;
+            this.dataGridViewTotal.Size = new System.Drawing.Size(545, 339);
+            this.dataGridViewTotal.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dataGridViewMatrix);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(545, 342);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "矩阵表";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewMatrix
+            // 
+            this.dataGridViewMatrix.AllowUserToAddRows = false;
+            this.dataGridViewMatrix.AllowUserToDeleteRows = false;
+            this.dataGridViewMatrix.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewMatrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMatrix.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewMatrix.Name = "dataGridViewMatrix";
+            this.dataGridViewMatrix.ReadOnly = true;
+            this.dataGridViewMatrix.RowTemplate.Height = 23;
+            this.dataGridViewMatrix.Size = new System.Drawing.Size(549, 339);
+            this.dataGridViewMatrix.TabIndex = 0;
+            this.dataGridViewMatrix.RowPostPaint += dataGridViewMatrix_RowPostPaint;
+            // 
+            // Index
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 359);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Index";
+            this.Text = "BusDataStats";
+            this.Load += new System.EventHandler(this.Index_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStats)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTotal)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMatrix)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewStats;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridViewTotal;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dataGridViewMatrix;
+    }
+}
+
